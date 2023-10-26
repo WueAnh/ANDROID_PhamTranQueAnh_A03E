0b7682d5ea0c78c9be976ba30cef1bcb3b9158fd
WueAnh <phamtranqueanh@gmail.com>
2023-10-26 11:40:39 +0700

    Lab5_PhamTranQueAnh_205051909

---
+++ Lab5_BasicStateCodelab/Lab5_BasicStateCodelab/app/src/main/java/com/example/lab5_basicstatecodelab/WaterCounter.kt
@@ -0,0 +1,53 @@
+package com.example.lab5_basicstatecodelab
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Row
+
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count, { count++ }, modifier)
+}
+
+/*@Composable
+fun StatefulCounter() {
+    var waterCount by remember { mutableStateOf(0) }
+
+    var juiceCount by remember { mutableStateOf(0) }
+
+    StatelessCounter(waterCount, { waterCount++ })
+    StatelessCounter(juiceCount, { juiceCount++ })
+}*/
+
+@Composable
+fun StatefulCounter() {
+    var count by remember { mutableStateOf(0) }
+
+    StatelessCounter(count, { count++ })
+   /* AnotherStatelessMethod(count, { count *= 2 }) */
+}
